1faa583168e7bdbeba1b14741d528209c65338b5
AASHISHAG <agarwalaashish20@gmail.com>
2019-08-16 12:14:55 +0200

    Updated Code

--- recipe_v2/cmd.sh
+++ recipe_v2/cmd.sh
@@ -2,11 +2,12 @@
 
 # This script is adapted from swbd Kaldi run.sh (https://github.com/kaldi-asr/kaldi
 # Copyright 2018 Kaldi developers (see: https://github.com/kaldi-asr/kaldi/blob/master/COPYING)
+# Change GPU and G2P path accordingly
 
 export train_cmd="utils/run.pl"
 export decode_cmd="utils/run.pl"
 export cuda_cmd="utils/run.pl -l gpu=1"
 export sequitur_g2p="/usr/aashish/g2p/g2p.py"
 
-export nJobs=4
-export nDecodeJobs=2
+export nJobs=28
+export nDecodeJobs=12
